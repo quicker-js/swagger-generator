5ddc05becc15c1eb728680bd195d6b0e1d0493e8
ranyunlong <549510622@qq.com>
2021-11-22 11:43:15 +0800

    update - Rebuild docs.

--- swagger-generator.config.js
+++ swagger-generator.config.js
@@ -2,8 +2,9 @@ const path = require('path');
 
 module.exports = {
   namespaces: {
-    v2: path.resolve("test", 'v2.json'),
-    v3: path.resolve('test', 'v3.json')
+    // v2: path.resolve("test", 'v2.json'),
+    // v3: path.resolve('test', 'v3.json'),
+    ss: 'http://58.33.188.228:21000/api/auth-service/v2/api-docs'
   },
   output: path.resolve('apis'),
   replaces: [
